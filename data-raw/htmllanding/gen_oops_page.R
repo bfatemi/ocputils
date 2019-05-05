908a083a37353b9d2e7a6a3e6d768a87c385454c
Bobby Fatemi <bfatemi07@gmail.com>
2019-05-04 20:46:33 -0700

    refreshed data

--- data-raw/htmllanding/gen_oops_page.R
+++ data-raw/htmllanding/gen_oops_page.R
@@ -5,4 +5,4 @@
 odir <- system.file("page_oops", package = "ocputils")
 oops_html <- paste0(readLines(paste0(odir, "/index.html")), collapse = "\n")
 
-usethis::use_data(oops_html)
+usethis::use_data(oops_html, overwrite = TRUE)
