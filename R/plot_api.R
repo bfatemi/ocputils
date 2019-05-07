9bad563f12f75a592fbb777313567ae259114e37
bfatemi <bfatemi07@gmail.com>
2019-05-06 23:28:00 -0700

    cleaned

--- R/plot_api.R
+++ R/plot_api.R
@@ -2,17 +2,9 @@
 #'
 #' @param p TBD
 #' @param hxp TBD
-#' @param h TBD
-#' @param w TBD
-#' @param res TBD
-#' @param show TBD
-#' 
-#' @import shiny
-#' @importFrom httr POST
-#' @importFrom htmltools htmlTemplate
+#'
 #' @importFrom sodium hex2bin bin2hex 
 #' @importFrom protolite serialize_pb unserialize_pb
-#' @importFrom utils browseURL
 #'
 #'@name plot_api
 NULL
@@ -31,36 +23,3 @@ hex2plot <- function(hxp){
   obj <- protolite::unserialize_pb(sodium::hex2bin(hxp))
   unserialize(charToRaw(obj))
 }
-
-#' @describeIn plot_api TBD
-#' @export
-plotpost <- function(p, h=700, w=700, res=160, show = TRUE){
-  # url <- ocputils::ocpu_icecube_url("publishPlot")
-  url <- "https://icecube.hpds.network/ocpu/library/ocputils/R/publishPlot"
-  r <- httr::POST(url, body = list(hxp = paste0("'", plot2hex(p), "'"), #plot2hex(p), 
-                                   height = h, 
-                                   width = w, 
-                                   res = res))
-  id <- r$headers$`x-ocpu-session`
-  api_path <- paste0("/", id, "/files/plot.png")
-  
-  if(show){
-    view_api(api_path)
-    return(api_path)
-  }else{
-    return(api_path)
-  }
-}
-
-
-# viewUI( publishUI("inst/login.html") )
-# 
-# 
-# ui <- basicPage(h1("Heyyy"))
-# 
-# script <- includeScript("R/widget/sigma.js")
-# style  <- includeCSS("R/widget/style.css")
-# 
-# id <- publish(ui, script, style)
-# 
-# viewUI(id)
--- R/zzz.R
+++
@@ -1,9 +0,0 @@
-
-# options(
-#   usethis.description = list(
-#     `Authors@R` = 'person(given = "Bobby", family = "Fatemi", role = c("aut", "cre"), email = "bfatemi07@gmail.com", comment = c(ORCID = "YOUR-ORCID-ID"))',
-#     License = "file LICENSE"
-#   )
-# )
-#
-# usethis::create_package("../ocputils")
